4b533f4143ebe51bb346c1d1c5f429df1b88c83f
abhi2496 <abhishekkoranne2@gmail.com>
2019-05-26 18:07:54 +0530

    Problem solved!

--- May/src/Wiggle_Walk.java
+++ May/src/Wiggle_Walk.java
@@ -1,8 +1,69 @@
+import java.util.Scanner;
 
 public class Wiggle_Walk {
 
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String ti = scan.nextLine();
+		int t = Integer.parseInt(ti);
+		for (int i = 0; i < t; i++) {
 
+			String line = scan.nextLine();
+			String[] nos = line.trim().split("\\s+");
+			int a[] = new int[nos.length];
+
+			for (int j = 0; j < nos.length; j++) {
+				a[j] = Integer.parseInt(nos[j]);
+			}
+
+			int visited[][] = new int[a[1]][a[2]];
+			for (int j = 0; j < a[1]; j++) {
+				for (int k = 0; k < a[2]; k++) {
+					visited[j][k] = 0;
+				}
+			}
+			
+			visited[--a[3]][--a[4]] = 1;
+			int x=a[3];
+			int y=a[4];
+			String instrucline = scan.nextLine();			
+			for (int k = 0; k < a[0]; k++) {
+
+
+//				for (int k = 0; k < instrucline.length(); k++) {
+					if (instrucline.charAt(k)=='E') {
+						while (visited[x][y+1]!=0) {
+							y++;
+						}
+						visited[x][++y]=1;
+					}
+					
+					if (instrucline.charAt(k)=='W') {
+						while (visited[x][y-1]!=0) {
+							y--;
+						}
+						visited[x][--y]=1;
+					}
+					
+					if (instrucline.charAt(k)=='N') {
+						while (visited[x-1][y]!=0) {
+							x--;
+						}
+						visited[--x][y]=1;
+					}
+					
+					if (instrucline.charAt(k)=='S') {
+						while (visited[x+1][y]!=0) {
+							x++;
+						}
+						visited[++x][y]=1;
+					}
+//				}
+			}
+			System.out.println("Case #"+ ++i +": "+ ++x+" "+ ++y);
+		}
+	
+		scan.close();
 	}
 
 }
